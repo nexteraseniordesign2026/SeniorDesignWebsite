3e9b03428beaf03edb9aaaa36bb9ec530e4b2839
justlooking88 <beachhouse376@gmail.com>
2026-02-19 19:42:50 -0500

    //changes made by Jess

--- config.example.js
+++ config.example.js
@@ -2,5 +2,5 @@
 // Copy this file to config.js and add your actual API key
 // config.js is gitignored and will not be committed to the repository
 
-window.GOOGLE_MAPS_API_KEY = 'YOUR_API_KEY_HERE';
+window.GOOGLE_MAPS_API_KEY = 'AIzaSyA76n8DEr2lwHQuWsYR0wnlFFwcoxLPUak';
 
